7f1c23601075710d78f690606f3024e2ede767f0
ryanckoch <ryanckoch@hp.com>
2015-05-02 16:00:40 -0600

    Corrected mysql host

--- update-realm.sh
+++ update-realm.sh
@@ -4,7 +4,7 @@ read -p 'IP Address of your realm: ' -r
 realmAddress=$REPLY
 
 
-mysql -u root -p << EOF
+mysql -u root -p -h mysql << EOF
 UPDATE 'realmlist'
 SET `address` = "$realmAddress"
 WHERE `realmlist`.`id` = 1;
